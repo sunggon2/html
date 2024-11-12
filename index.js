eb7960a048a5c292e756a7e59c25dd53291a3766
sunggon2 <kim3pyo1@naver.com>
2024-11-12 20:17:36 +0900

    6th map

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
+let map;
+
 async function initMap() {
-    // Request needed libraries.
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const myLatlng = { lat: -25.363, lng: 131.044 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatlng,
-      mapId: "DEMO_MAP_ID",
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  let center = new google.maps.LatLng(52.369358, 4.889258);
+
+  map = new Map(document.getElementById("map"), {
+    center: center,
+    zoom: 11,
+    mapId: "DEMO_MAP_ID",
+  });
+  nearbySearch();
+}
+
+async function nearbySearch() {
+  //@ts-ignore
+  const { Place, SearchNearbyRankPreference } = await google.maps.importLibrary(
+    "places",
+  );
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  // Restrict within the map viewport.
+  let center = new google.maps.LatLng(52.369358, 4.889258);
+  const request = {
+    // required parameters
+    fields: ["displayName", "location", "businessStatus"],
+    locationRestriction: {
+      center: center,
+      radius: 500,
+    },
+    // optional parameters
+    includedPrimaryTypes: ["restaurant"],
+    maxResultCount: 5,
+    rankPreference: SearchNearbyRankPreference.POPULARITY,
+    language: "en-US",
+    region: "us",
+  };
+  //@ts-ignore
+  const { places } = await Place.searchNearby(request);
+
+  if (places.length) {
+    console.log(places);
+
+    const { LatLngBounds } = await google.maps.importLibrary("core");
+    const bounds = new LatLngBounds();
+
+    // Loop through and get all the results.
+    places.forEach((place) => {
+      const markerView = new AdvancedMarkerElement({
+        map,
+        position: place.location,
+        title: place.displayName,
+      });
+
+      bounds.extend(place.location);
+      console.log(place);
     });
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      position: myLatlng,
-      map,
-      title: "Click to zoom",
-    });
-  
-    map.addListener("center_changed", () => {
-      // 3 seconds after the center of the map has changed, pan back to the
-      // marker.
-      window.setTimeout(() => {
-        map.panTo(marker.position);
-      }, 3000);
-    });
-    marker.addListener("click", () => {
-      map.setZoom(8);
-      map.setCenter(marker.position);
-    });
+    map.fitBounds(bounds);
+  } else {
+    console.log("No results");
   }
-  
-  initMap();
+}
+
+initMap();
